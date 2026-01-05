142e5b301799ecd15ad3d1681073baa31b79dead
Cristian <crisanrova@gmail.com>
2026-01-05 13:41:58 -0500

    Actualizacion

--- app/Http/Controllers/OrderServiceController.php
+++ app/Http/Controllers/OrderServiceController.php
@@ -74,10 +74,10 @@ class OrderServiceController extends Controller
                     'model' => $validated['vehicle_model'],
                     'year' => $validated['vehicle_year'],
                     'cilindraje' => $validated['vehicle_cilindraje'],
-                    'vin' => strtoupper($validated['vehicle_vin']),
-                    'motor' => $validated['vehicle_motor'],
+                    'vin' => !empty($validated['vehicle_vin']) ? strtoupper($validated['vehicle_vin']) : null,
+                    'motor' => !empty($validated['vehicle_motor']) ? $validated['vehicle_motor'] : null,
                     'kilometraje' => $validated['vehicle_kilometraje'],
-                    'observaciones' => $validated['vehicle_observaciones'] ?? null,
+                    'observaciones' => !empty($validated['vehicle_observaciones']) ? $validated['vehicle_observaciones'] : null,
                 ]
             );
 
@@ -163,10 +163,10 @@ class OrderServiceController extends Controller
                     'model' => $validated['vehicle_model'],
                     'year' => $validated['vehicle_year'],
                     'cilindraje' => $validated['vehicle_cilindraje'],
-                    'vin' => strtoupper($validated['vehicle_vin']),
-                    'motor' => $validated['vehicle_motor'],
+                    'vin' => !empty($validated['vehicle_vin']) ? strtoupper($validated['vehicle_vin']) : null,
+                    'motor' => !empty($validated['vehicle_motor']) ? $validated['vehicle_motor'] : null,
                     'kilometraje' => $validated['vehicle_kilometraje'],
-                    'observaciones' => $validated['vehicle_observaciones'] ?? null,
+                    'observaciones' => !empty($validated['vehicle_observaciones']) ? $validated['vehicle_observaciones'] : null,
                 ]
             );
 
