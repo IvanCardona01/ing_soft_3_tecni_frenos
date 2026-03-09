95ecda0c1ebc10185a97c096423fc34f2a022201
Ivan Cardona <2021ivancardona@gmail.com>
2026-03-08 20:46:15 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans antialiased">
+    <div class="min-h-screen flex items-center justify-center">
+        <h1 class="text-2xl">{{ config('app.name') }}</h1>
+    </div>
+</body>
+</html>
